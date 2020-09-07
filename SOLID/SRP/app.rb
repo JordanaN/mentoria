afceb5e266b34900ca6540ae338b21da524e769b
JordanaN <jordanasnogueira@gmail.com>
2020-09-07 17:04:35 -0300

    corrige app

--- SOLID/SRP/app.rb
+++ SOLID/SRP/app.rb
@@ -1,17 +1,17 @@
 require_relative 'employee'
 
-jojo = Employee.new(full_name: 'Jordana Nogueira', birthdate: '06/01/1988', salary: 5_000.0)
-puts jojo.full_name
-puts jojo.birthdate
-puts jojo.salary
+employee = Employee.new(full_name: 'Jordana Nogueira', birthdate: '06/01/1988', salary: 5_000.0)
+puts employee.full_name
+puts employee.birthdate
+puts employee.salary
 
-puts jojo.first_name
-puts jojo.last_name
-puts jojo.age
-puts jojo.salary_calculation
+puts employee.first_name
+puts employee.last_name
+puts employee.age
+puts employee.salary_calculation
 
-instance_employee = jojo.save
+instance_employee = employee.save
 puts "employee criado #{instance_employee}"
 
-employee = jojo.get_employee(instance_employee[:id])
-puts "select de employee #{employee}"
+result = employee.get_employee(instance_employee[:id])
+puts "select de employee #{result}"
